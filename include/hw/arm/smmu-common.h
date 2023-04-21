c90ba26fa95237e4e09591c791931eed8575a69d
Nicolin Chen <nicolinc@nvidia.com>
2023-04-21 15:13:53 -0700

    hw/arm/smmuv3: Add smmu_dev_install_nested_ste() for CFGI_STE
    Call smmu_dev_install_nested_ste and eventually down to IOMMU_HWPT_ALLOC
    ioctl for a nested HWPT allocation.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/hw/arm/smmu-common.h
+++ include/hw/arm/smmu-common.h
@@ -138,6 +138,12 @@ typedef struct SMMUViommu {
     QLIST_ENTRY(SMMUViommu) next;
 } SMMUViommu;
 
+typedef struct SMMUVdev {
+    SMMUViommu *vsmmu;
+    IOMMUFDVdev *core;
+    uint32_t sid;
+}SMMUVdev;
+
 typedef struct SMMUS1Hwpt {
     void *smmu;
     IOMMUFDBackend *iommufd;
@@ -154,6 +160,7 @@ typedef struct SMMUDevice {
     IOMMUMemoryRegion  iommu;
     HostIOMMUDeviceIOMMUFD *idev;
     SMMUViommu         *viommu;
+    SMMUVdev           *vdev;
     SMMUS1Hwpt         *s1_hwpt;
     AddressSpace       as;
     AddressSpace       as_sysmem;
