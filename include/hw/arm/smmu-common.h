4b9b7d79d932d0ae46d6491bab1929f9a3cab9a8
Nicolin Chen <nicolinc@nvidia.com>
2023-12-07 20:04:47 +0000

    hw/arm/smmu-common: Return sysmem if stage-1 is bypassed
    When nested translation is enabled, there are 2-stage translation occuring
    to two different address spaces: stage-1 in the iommu as, while stage-2 in
    the system as.
    
    If a device attached to the vSMMU doesn't enable stage-1 translation, e.g.
    vSTE sets to Config=Bypass, the system as should be returned, so QEMU can
    set up system memory mappings onto the stage-2 page table. This is crucial
    for an iommufd enabled VFIO device as the VFIO core code would register an
    iommu notifier and replay the address space which should be bypassed for a
    nested translation case.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/hw/arm/smmu-common.h
+++ include/hw/arm/smmu-common.h
@@ -156,6 +156,7 @@ typedef struct SMMUDevice {
     SMMUViommu         *viommu;
     SMMUS1Hwpt         *s1_hwpt;
     AddressSpace       as;
+    AddressSpace       as_sysmem;
     uint32_t           cfg_cache_hits;
     uint32_t           cfg_cache_misses;
     QLIST_ENTRY(SMMUDevice) next;
@@ -183,7 +184,9 @@ struct SMMUState {
     /* <private> */
     SysBusDevice  dev;
     const char *mrtypename;
+    MemoryRegion root;
     MemoryRegion iomem;
+    MemoryRegion sysmem;
 
     /* Nested SMMU */
     bool nested;
