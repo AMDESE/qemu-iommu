118116e7e4743eb911fecb27bc9f39d6a0a45ba9
Nicolin Chen <nicolinc@nvidia.com>
2024-04-29 21:26:41 +0000

    hw/arm/smmuv3: Add missing STE invalidation
    Multitple STEs can be invalidated in a range via SMMU_CMD_CFGI_STE_RANGE
    or SMMU_CMD_CFGI_ALL command.
    
    Add the missing STE invalidation in this pathway.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- hw/arm/smmu-internal.h
+++ hw/arm/smmu-internal.h
@@ -141,4 +141,10 @@ typedef struct SMMUIOTLBPageInvInfo {
     uint64_t mask;
 } SMMUIOTLBPageInvInfo;
 
+typedef struct SMMUSIDRange {
+    SMMUState *state;
+    uint32_t start;
+    uint32_t end;
+} SMMUSIDRange;
+
 #endif
