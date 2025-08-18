hisilicon/qemu

hw/iommufd: introduce IOMMUFDDevice and targeted interface
In nested translation set up, vIOMMU is going to have more iommufd
related operations like getting hw IOMMU information, allocate hwpt
for a device, and attach device to hwpt. It is necessary to have an
object to represent a device in iommufd and can be used by vIOMMU to
conduct the above operations.

IOMMUFDDevice is used as a communication interface between VFIO and
vIOMMU. This is willingly not a QOM object because we don't want it
to be visible from the user interface.

Introduce a helper iommufd_device_init to initialize IOMMUFDDevice
and target ops.

Originally-by: Yi Liu <yi.l.liu@intel.com>
Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>
Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

include/sysemu/iommufd_device.h
@@ -1,0 +1,43 @@
+/*
+ * IOMMUFD Device
+ *
+ * Copyright (C) 2023 Intel Corporation.
+ *
+ * Authors: Yi Liu <yi.l.liu@intel.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef SYSEMU_IOMMUFD_DEVICE_H
+#define SYSEMU_IOMMUFD_DEVICE_H
+
+#include <linux/iommufd.h>
+#include "sysemu/iommufd.h"
+
+typedef struct IOMMUFDDevice IOMMUFDDevice;
+
+typedef struct IOMMUFDDeviceOps {
+    int (*attach_hwpt)(IOMMUFDDevice *idev, uint32_t hwpt_id);
+    int (*detach_hwpt)(IOMMUFDDevice *idev);
+} IOMMUFDDeviceOps;
+
+/* This is an abstraction of host IOMMUFD device */
+struct IOMMUFDDevice {
+    IOMMUFDDeviceOps *ops;
+    IOMMUFDBackend *iommufd;
+    uint32_t dev_id;
+    uint32_t def_hwpt_id;
+    bool initialized;
+};
+
+int iommufd_device_attach_hwpt(IOMMUFDDevice *idev, uint32_t hwpt_id);
+int iommufd_device_detach_hwpt(IOMMUFDDevice *idev);
+int iommufd_device_get_info(IOMMUFDDevice *idev,
+                            enum iommu_hw_info_type *type,
+                            uint32_t len, void *data);
+void iommufd_device_init(void *_idev, size_t instance_size,
+                         IOMMUFDDeviceOps *ops, IOMMUFDBackend *iommufd,
+                         uint32_t dev_id, uint32_t hwpt_id);
+void iommufd_device_destroy(IOMMUFDDevice *idev);
+
+#endif

AMDESE/qemu-iommu

include/sysemu/iommufd_device.h
@@ -1,0 +1,43 @@
+/*
+ * IOMMUFD Device
+ *
+ * Copyright (C) 2023 Intel Corporation.
+ *
+ * Authors: Yi Liu <yi.l.liu@intel.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef SYSEMU_IOMMUFD_DEVICE_H
+#define SYSEMU_IOMMUFD_DEVICE_H
+
+#include <linux/iommufd.h>
+#include "system/iommufd.h"
+
+typedef struct IOMMUFDDevice IOMMUFDDevice;
+
+typedef struct IOMMUFDDeviceOps {
+    int (*attach_hwpt)(IOMMUFDDevice *idev, uint32_t hwpt_id);
+    int (*detach_hwpt)(IOMMUFDDevice *idev);
+} IOMMUFDDeviceOps;
+
+/* This is an abstraction of host IOMMUFD device */
+struct IOMMUFDDevice {
+    IOMMUFDDeviceOps *ops;
+    IOMMUFDBackend *iommufd;
+    uint32_t dev_id;
+    uint32_t def_hwpt_id;
+    bool initialized;
+};
+
+int iommufd_device_attach_hwpt(IOMMUFDDevice *idev, uint32_t hwpt_id);
+int iommufd_device_detach_hwpt(IOMMUFDDevice *idev);
+int iommufd_device_get_info(IOMMUFDDevice *idev,
+                            enum iommu_hw_info_type *type,
+                            uint32_t len, void *data);
+void iommufd_device_init(void *_idev, size_t instance_size,
+                         IOMMUFDDeviceOps *ops, IOMMUFDBackend *iommufd,
+                         uint32_t dev_id, uint32_t hwpt_id);
+void iommufd_device_destroy(IOMMUFDDevice *idev);
+
+#endif