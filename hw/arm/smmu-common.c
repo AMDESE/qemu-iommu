f4027b3814032d3c912291d29ec5df73d4cb3852
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2025-10-14 01:47:20 +0000

    DEBUG

--- hw/arm/smmu-common.c
+++ hw/arm/smmu-common.c
@@ -1220,6 +1220,7 @@ free:
     return -EINVAL;
 }
 
+//FOR HWPT
 int smmu_hwpt_invalidate_cache(SMMUS1Hwpt *s1_hwpt, uint32_t type, uint32_t len,
                                uint32_t *num, void *reqs)
 {
@@ -1231,6 +1232,7 @@ int smmu_hwpt_invalidate_cache(SMMUS1Hwpt *s1_hwpt, uint32_t type, uint32_t len,
                                             type, len, num, reqs);
 }
 
+//FOR VIOMMU
 int smmu_viommu_invalidate_cache(IOMMUFDViommu *viommu, uint32_t type,
                                  uint32_t len, uint32_t *num, void *reqs)
 {
