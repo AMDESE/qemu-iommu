ce177666a473e8d9611a458d370e6e90c4149636
Vasant Hegde <vasant.hegde@amd.com>
2026-04-10 10:58:58 +0530

    hw/i386: Add secure vIOMMU model
    Add separate model for secure vIOMMU. Eventually we will enhance
    amd-viommu model itself.
    
    Note:
      - We have difference between Global EFR (comes from IVRS) and per
        IOMMU EFR. This works for now, but eventually we should fix this.
      - Need to review the way we find the gdevice id in amdvi_alloc_vdev()
      - There is difference in how we track hwpt_id etc. This needs to be
        reworked once amd_viommu rework is complete.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -354,6 +354,7 @@ static void pc_q35_machine_options(MachineClass *m)
     m->no_parallel = !module_object_class_by_name(TYPE_ISA_PARALLEL);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_VIOMMU_DEVICE);
+    machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_SVIOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_INTEL_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_RAMFB_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_VMBUS_BRIDGE);
