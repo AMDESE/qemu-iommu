AMDESE/qemu

commit a90b68df5d0daa017dd9e90e8eddd71ac589c29c

pci: Add helper to search PCI device by its [Domain:]Bus:Dev.Fn
Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

hw/pci/pci.c
@@ -2785,6 +2785,24 @@ int pci_qdev_find_device(const char *id, PCIDevice **pdev)
     return rc;
 }
 
+int pci_qdev_find_device_by_pciid(int domid, int busno, int32_t devfn, PCIDevice **pdev)
+{
+    PCIHostState *host_bridge;
+    int n = 0;
+
+    QLIST_FOREACH(host_bridge, &pci_host_bridges, next) {
+        if (n == domid) {
+            *pdev = pci_find_device(host_bridge->bus, busno, devfn);
+            if (*pdev) {
+                return 0;
+            }
+        }
+        ++n;
+    }
+
+    return -ENODEV;
+}
+
 MemoryRegion *pci_address_space(PCIDevice *dev)
 {
     return pci_get_bus(dev)->address_space_mem;

include/hw/pci/pci.h
@@ -371,6 +371,7 @@ const char *pci_root_bus_path(PCIDevice *dev);
 bool pci_bus_bypass_iommu(PCIBus *bus);
 PCIDevice *pci_find_device(PCIBus *bus, int bus_num, uint8_t devfn);
 int pci_qdev_find_device(const char *id, PCIDevice **pdev);
+int pci_qdev_find_device_by_pciid(int domid, int busno, int32_t devfn, PCIDevice **pdev);
 void pci_bus_get_w64_range(PCIBus *bus, Range *range);
 
 void pci_device_deassert_intx(PCIDevice *dev);

AMDESE/qemu-iommu

commit 6c43f9f1d46951d653c827091f84d3d110fb1b88

hw/pci: Fix helper function to search PCI device by BDF
Fixes: a90b68df5d0d ("pci: Add helper to search PCI device by its [Domain:]Bus:Dev.Fn")
Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

hw/pci/pci.c
@@ -2794,16 +2794,20 @@ int pci_qdev_find_device(const char *id, PCIDevice **pdev)
 int pci_qdev_find_device_by_pciid(int domid, int busno, int32_t devfn, PCIDevice **pdev)
 {
     PCIHostState *host_bridge;
-    int n = 0;
 
     QLIST_FOREACH(host_bridge, &pci_host_bridges, next) {
-        if (n == domid) {
-            *pdev = pci_find_device(host_bridge->bus, busno, devfn);
-            if (*pdev) {
-                return 0;
-            }
-        }
-        ++n;
+        PCIDevice *p;
+        PCIBus *pci_bus = pci_find_bus_nr(host_bridge->bus, busno);
+
+        if (!pci_bus)
+            continue;
+
+        p = pci_bus->devices[devfn];
+        if (!p)
+            continue;
+
+        *pdev = p;
+        return 0;
     }
 
     return -ENODEV;